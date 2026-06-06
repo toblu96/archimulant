69b2545b29896392f4952f92478bdf70e9d428f9
Tobias Blum <tobiasblum02@gmail.com>
2026-06-06 14:41:36 +0200

    fix: server auth middleware only for api routes

--- server/middleware/01.auth.ts
+++ server/middleware/01.auth.ts
@@ -1,6 +1,8 @@
 import { AppError } from '~~/server/domain/errors'
 
 export default defineEventHandler((event) => {
+  if (!event.path.startsWith('/api')) return
+
   const { auth } = getRouteRules(event)
   if (auth === false) return
   if (!event.context.scope?.currentUser) {
